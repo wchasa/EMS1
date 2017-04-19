8eb8f468dcd9bcb89abf3483431ef2c54ed9f7c3
wch <wchasa@gmail.com>
2017-04-19 16:22:43 +0800

    prunch3.2

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-./test ./yst03r.fasta 6 2
-./test ./yst03r.fasta 7 2
-./test ./yst03r.fasta 8 2
-./test ./yst03r.fasta 9 2
-./test ./yst03r.fasta 10 2
+#./test ./yst03r.fasta 6 2
+#./test ./yst03r.fasta 7 2
+#./test ./yst03r.fasta 8 2
+#./test ./yst03r.fasta 9 2
+#./test ./yst03r.fasta 10 2
+./test ./yst03r.fasta 13 2
 
